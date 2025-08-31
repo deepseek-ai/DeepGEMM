55c79433841fc0bb50aed2dd0a51a85800058a05
Eric Wong <huangchun50@gmail.com>
2025-08-31 15:35:18 +0800

    more.

--- tests/test_signal_gemm.py
+++ tests/test_signal_gemm.py
@@ -92,7 +92,10 @@ def test_m_grouped_gemm_masked() -> None:
         # Test correctness
         for i in range(2):
             a, b, masked_m, d, ref_d = generate_m_grouped_masked(num_groups, max_m, expected_m_per_group, n, k, use_ue8m0=use_ue8m0)
+            origin_sms = deep_gemm.get_num_sms()
+            deep_gemm.set_num_sms(origin_sms - 3)
             deep_gemm.m_grouped_fp8_gemm_nt_masked(a, b, d, masked_m, expected_m_per_group, disable_ue8m0_cast=disable_ue8m0_cast)
+            deep_gemm.set_num_sms(origin_sms)
             for j in range(num_groups):
                 diff = calc_diff(d[j, :masked_m[j].item()], ref_d[j, :masked_m[j].item()])
                 assert diff < 0.001, f'{max_m=}, {n=}, {k=}, {j=}, masked_m={masked_m[j]}, {kernel_opt}, {num_groups=}, {diff:.5f}'
