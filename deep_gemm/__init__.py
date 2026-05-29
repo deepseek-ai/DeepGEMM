fed51f1051732087fdfcd1ac2be1b901dc5777d4
yinding <yinding@bytedance.com>
2026-05-29 15:59:48 +0800

    add an alias name

--- deep_gemm/__init__.py
+++ deep_gemm/__init__.py
@@ -251,6 +251,7 @@ try:
         (a, a_sf), (b, b_sf) = _parse_tensor_or_tuple(a), _parse_tensor_or_tuple(b)
         return _C.m_grouped_fp8_fp4_gemm_nt_masked(a, a_sf, b, b_sf, d, masked_m, expected_m, recipe, recipe_a, recipe_b, compiled_dims, disable_ue8m0_cast)
 
+    m_grouped_fp8_gemm_nt_masked = m_grouped_fp8_fp4_gemm_nt_masked
     fp8_m_grouped_gemm_nt_masked = m_grouped_fp8_fp4_gemm_nt_masked
     bf16_m_grouped_gemm_nt_masked = None
 
